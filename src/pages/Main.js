343c0bacf7a1df4a89bd180604d8d11aa573faaa
YUNH7 <yunny126@gmail.com>
2023-04-19 09:51:27 +0900

    ADD: Main 디자인

---
+++ src/pages/Main.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import PageTemplate from "../components/PageTemplate";
+
+const P = styled.p`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+`;
+
+function Main() {
+  return (
+    <PageTemplate pageName="투두">
+      <P>상단 탭을 눌러주세요</P>
+    </PageTemplate>
+  );
+}
+
+export default Main;
--- src/utilities/router.js
+++ src/utilities/router.js
@@ -7,10 +7,12 @@ import App from "../App";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 import ToDoList from "../pages/ToDoList";
+import Main from "../pages/Main";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route path="" element={<Main />} />
       <Route path="signup" element={<SignUp />} />
       <Route path="signin" element={<SignIn />} />
       <Route path="todo" element={<ToDoList />} />
